ed820cbd609761ad6f1b3a90059de99cb70edc6b
Adrián Pérez Salinas <adrian.perez@bsc.es>
2020-04-24 15:38:17 +0200

    Arxiv code

--- arxiv_code/main.py
+++ arxiv_code/main.py
@@ -3,19 +3,20 @@ from errors import errors
 S0 = 2
 K = 1.9
 sig = 0.4
-
 r = 0.05
 T = 0.1
-
 data = (S0, sig, r, T, K)
 
 max_error_gate = 0.005
 
-steps = 11
+steps = 101
 
 Err = errors(data, max_error_gate, steps)
 bins = 8
 error_name = 'bitflip'
-repeats = 10
+repeats = 100
 measure=False
+
+Err.compute_save_errors_unary(bins, error_name, repeats)
+Err.compute_save_errors_binary(bins, error_name, repeats)
 Err.paint(bins, error_name, repeats)
